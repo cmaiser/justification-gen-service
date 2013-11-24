b6ee65529107129fca29d781a35479f39afb7826
cmaiser <nnagflar@gmail.com>
2013-11-23 19:54:03 -0500

    import scope fix

--- excusegenservice/urls.py
+++ excusegenservice/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from excusegenservice import locationResolver
+from excusegenservice.views import locationResolver
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
