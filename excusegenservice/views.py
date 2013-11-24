b6698a6445485958f3007a2b42f56f99f16452ff
cmaiser <nnagflar@gmail.com>
2013-11-23 21:26:17 -0500

    location resolver code

--- excusegenservice/views.py
+++ excusegenservice/views.py
@@ -1,7 +1,24 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.views.decorators.csrf import csrf_exempt
+import urllib2
 
 #TODO, use csrf in the future.
 @csrf_exempt
 def locationResolver(request):
-  return HttpResponse("Got it")
+  
+  if request.is_ajax():
+    try:
+      latitude = str(request.POST['lat'])
+      longitude = str(request.POST['lon'])
+      url = "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + latitude + "," + longitude + "&sensor=false"
+      
+      try:
+	response = urllib2.urlopen(url)
+	return HttpResponse(response.read())
+      except:
+	return HttpResponse("Error: could not resolve location!")
+      
+    except:
+      return HttpResponse("Error: bad request!")
+  else:
+    raise Http404
