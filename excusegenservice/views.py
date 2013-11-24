5b51d07c44061b9b85b77d2a35a7d8b512d49a8f
cmaiser <nnagflar@gmail.com>
2013-11-23 20:22:14 -0500

    removed csrf check... will fix later

--- excusegenservice/views.py
+++ excusegenservice/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.views.decorators.csrf import csrf_exempt
 
+#TODO, use csrf in the future.
+@csrf_exempt
 def locationResolver(request):
   return HttpResponse("Got it")
