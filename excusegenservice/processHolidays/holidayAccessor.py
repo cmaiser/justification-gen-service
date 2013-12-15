0a4211d2089d74902d53c1d4eec27c9ccd5cdc10
cmaiser <nnagflar@gmail.com>
2013-12-14 23:38:45 -0500

    changed holidays to local data

--- excusegenservice/processHolidays/holidayAccessor.py
+++ excusegenservice/processHolidays/holidayAccessor.py
@@ -1,26 +1,21 @@
-import urllib2
-import json
+#!/usr/bin/python
 
-def getHolidays(day, month, year, logger):
-  
-  url = "http://holidayapi.com/v1/holidays?country=US&year=" + year + "&month=" + month + "&day=" + day
-  hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-       'Accept-Encoding': 'none',
-       'Accept-Language': 'en-US,en;q=0.8',
-       'Connection': 'keep-alive'}
-  
-  logger.debug("holidayAccessor.getHolidays - Accessing " + url)
+import re
+from django.conf import settings
 
-  try:
+def getHolidays(day, month, year):
   
-    req = urllib2.Request(url, headers=hdr)
-    response = urllib2.urlopen(req)
-    data = json.load(response)
-    return data
+  holidayList = []
+    
+  path = settings.PROJECT_ROOT + "/config/holidays.txt"
   
-  except urllib2.HTTPError, e:
-    logger.error('holidayAccessor.getHolidays HTTPError = ' + str(e.code))
-  except urllib2.URLError, e:
-    logger.error('holidayAccessor.getHolidays URLError = ' + str(e.reason))
+  logger.debug("retrieving holidays - file path: " + path)
+  
+  for line in open(path):
+    matchObj = re.match(r'(\d{4})\s(\d{2})\s(\d{2})\s(.+)', line)
+    
+    if matchObj:
+      if matchObj.group(1) == year and matchObj.group(2) == month and matchObj.group(3) == day:
+        holidayList.add(matchObj.group(4))
+
+  return holidayList
