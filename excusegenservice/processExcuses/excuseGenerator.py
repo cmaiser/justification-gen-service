7f2e4d071a4cd50eb61a2598d2b102e7cdcfd9f6
cmaiser <nnagflar@gmail.com>
2013-12-21 02:17:23 -0500

    less ridiculous

--- excusegenservice/processExcuses/excuseGenerator.py
+++ excusegenservice/processExcuses/excuseGenerator.py
@@ -85,7 +85,7 @@ def generateExcusesFromData(tweets, traffic, weather, holidays, logger):
   if len(weather["alerts"]) > 0:
     weatherExcuse = weather["alerts"][0]["description"]
   else:
-    weatherExcuse = "I feel horrible about how I handled a recent situation and how I treated someone I care about."
+    weatherExcuse = "It's too nice of a day!"
   
   results["excuses"]["weatherExcuse"]["weather"] = weather
   results["excuses"]["weatherExcuse"]["text"] = weatherExcuse
