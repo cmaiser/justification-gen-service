19328617d8797767574470c002dc9b795b728f1a
cmaiser <nnagflar@gmail.com>
2013-12-22 18:27:15 -0500

    no traffic no bug

--- excusegenservice/processExcuses/excuseGenerator.py
+++ excusegenservice/processExcuses/excuseGenerator.py
@@ -78,7 +78,7 @@ def generateExcusesFromData(tweets, traffic, weather, holidays, logger):
   if "shortDesc" in topIncident.keys():
     results["excuses"]["trafficExcuse"]["text"] = "I am stuck in my car.  " + topIncident["shortDesc"]
   else:
-    results["excuses"]["trafficExcuse"]["text"] = "I am stuck in my car with a flat.  No traffic incidents found."
+    results["excuses"]["trafficExcuse"]["text"] = "You found a bug.  I'm sorry.  About everything."
   
   #process weather excuses
   
