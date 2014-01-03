dd9b74ff16afd1289b30a128adea8c8cfa63deb4
cmaiser <nnagflar@gmail.com>
2014-01-02 21:52:06 -0500

    comparing strings should be the same length

--- excusegenservice/processHolidays/holidayAccessor.py
+++ excusegenservice/processHolidays/holidayAccessor.py
@@ -4,13 +4,19 @@ from django.conf import settings
 def getHolidays(day, month, year, logger):
   
   holidayList = []
+  
+  if len(day) == 1:
+    day = "0" + day
+  
+  if len(month) == 1:
+    month = "0" + month
     
   path = settings.PROJECT_ROOT + "/config/holidays.txt"
   
   logger.debug("retrieving holidays - file path: " + path)
   
   for line in open(path):
-    matchObj = re.match(r'(\d{4})\s(\d{2})\s(\d{2})\s(.+)', line)
+    matchObj = re.match(r'(\d{4})\s(\d+)\s(\d+)\s(.+)', line)
     
     if matchObj:
       if matchObj.group(1) == year and matchObj.group(2) == month and matchObj.group(3) == day:
